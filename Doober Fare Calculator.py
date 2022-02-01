ea4f4820ac8709f5c6ab08cffc0a1c3466be2ffc
bernardmonteiro <bernardmonteiro@hotmail.com>
2022-02-01 16:50:12 -0300

    Add files via upload

---
+++ Doober Fare Calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+ride_type = "Black"
+credits = 4
+
+ride_price = 0
+final_price = 0
+
+if ride_type == "DooberX":
+  ride_price = 20.5
+elif ride_type == "Black":
+  ride_price = 37.9
+else:
+  ride_price = 18.7
+
+print("Ride price:")
+print(ride_price)
+
+if credits > 0:
+  final_price = ride_price - credits
+
+print("Final price:")
+print(final_price)
+
