905d4240ecfabebeed3c3744d8dbe9b13cc4b2a9
bernardmonteiro <bernardmonteiro@hotmail.com>
2022-02-01 16:54:38 -0300

    Add files via upload

---
+++ User Database.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+user_added = True
+user = "Becca"
+
+if user_added == False:
+  print(f"Add {user} to database")
+  user_added = True
+print(f"Database has user: {user}")
+
