50c9ead088684ff61c09102350e96f42b5b6617c
bernardmonteiro <bernardmonteiro@hotmail.com>
2022-02-01 16:47:15 -0300

    Add files via upload

---
+++ Survey Data Insights.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+frequency = "once a week"
+intensity = "low"
+
+highly_active = frequency == "daily"
+print("Highly active user:")
+print(highly_active)
+
+high_intensity = intensity == "high"
+print("High intensity sports:")
+print(high_intensity)
+
